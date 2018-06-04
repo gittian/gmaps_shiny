618456d8498f4eb7e5eafc6ed5b6902d77253f36
user14 <gittian@users.noreply.github.com>
2018-06-04 15:26:21 +0530

    Create dependency.R

---
+++ dependency.R
@@ -0,0 +1,16 @@
+if (!require(shiny)){install.packages("shiny")}
+if (!require(geosphere)){install.packages("geosphere")}
+if (!require(htmltools)){install.packages("htmltools")}
+if (!require(leaflet)){install.packages("leaflet")}
+if (!require(data.table)){install.packages("data.table")}
+if (!require(plotGoogleMaps)){install.packages("plotGoogleMaps")}
+if (!require(reshape)){install.packages("plotGoogleMaps")}
+
+library(shiny)
+library(geosphere)
+library(htmltools)
+library(leaflet)
+library(data.table)
+library(plotGoogleMaps)
+library(reshape)
+library(proxy)
