91bde54a1e9c14c33c54ad8f274c7d969be3d9d8
user14 <gittian@users.noreply.github.com>
2018-06-04 15:26:47 +0530

    Update server.R

--- server.R
+++ server.R
@@ -1,11 +1,3 @@
-if (!require(shiny)){install.packages("shiny")}
-if (!require(geosphere)){install.packages("geosphere")}
-if (!require(htmltools)){install.packages("htmltools")}
-if (!require(leaflet)){install.packages("leaflet")}
-if (!require(data.table)){install.packages("data.table")}
-if (!require(plotGoogleMaps)){install.packages("plotGoogleMaps")}
-if (!require(reshape)){install.packages("plotGoogleMaps")}
-
 library(shiny)
 library(geosphere)
 library(htmltools)
